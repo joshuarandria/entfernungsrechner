ddb06707336ba4e1345736763c6c7a25db87cf01
Randria, Joshua - (507927) <joshua.randria@alstomgroup.com>
2023-06-04 20:27:19 +0200

    v2

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # entfernungsrechner
+
+Code as answer for the Case Study Sprintstarter-Programm (see pdf)
+
+To run the app:
+cd /path/to/db-entfernungsrechner
+mvn clean install
+./mvnw spring-boot:run
+
+
+In Web Browser:
+http://localhost:8080/api/v1/distance/{from}/{to}
+
+from: DS100 code for a station part of the "FernVerkhehr" (Long distance train station)
+e.g. http://localhost:8080/api/v1/distance/FF/BLS
+
+
+If mvn does not work use .jar:
+java -jar target/rest-service-app-0.0.1-SNAPSHOT.jar
--- app/src/main/java/com/example/restservice/DistanceController.java
+++ app/src/main/java/com/example/restservice/DistanceController.java
@@ -14,13 +14,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class DistanceController {
 
 	public record BahnhofInfo(String name, Double laenge, Double breite) {}
 
 	@GetMapping("/api/v1/distance/{from}/{to}")
-	public Distance distance(@PathVariable("from") String from, @PathVariable("to") String to) {
+	public Distance distance(@PathVariable("from") String from, @PathVariable("to") String to) throws Exception {
 		BahnhofInfo fromBhf = getBhfInfo(from);
 		BahnhofInfo toBhf = getBhfInfo(to);
 		long distance = computeDistance(fromBhf, toBhf);
@@ -28,41 +33,55 @@ public class DistanceController {
 		return new Distance(fromBhf.name(), toBhf.name(), distance, "km");
 	}
 
-	private BahnhofInfo getBhfInfo(String dscode) {
-		System.out.println("getBhfInfo("+dscode+")");
-		// String filePath = "D_test.csv";
+	/**
+	Function getBhfInfo
+	Input: String dscode corresponding to a specific Bahnhof
+	Output: BahnHofInfo with relevant information on the associated Bahnhof
+	*/
+	private BahnhofInfo getBhfInfo(String dscode) throws Exception {
+		//System.out.println("getBhfInfo("+dscode+")");
 		String filePath = "D_Bahnhof_2020_alle.csv";
 
+		// Fetch data in the csv file
 		try (Reader reader = new FileReader(filePath);
              CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader().withDelimiter(';'))) {
 
+			// Iterate on every row to find the corresponding DS100 code
 			for (CSVRecord csvRecord : csvParser) {
 				String ds100 = csvRecord.get("DS100");
 				String verkehr = csvRecord.get("Verkehr");
 				if (ds100.equals(dscode)) {
-					System.out.println("dscode: "+dscode);
-					System.out.println("ds100: "+ds100);
-					if (verkehr.equals("FV")) { // interested only if Fernverkehr
+					if (verkehr.equals("FV")) { // filer on Fernverkehr
 						String name = csvRecord.get("NAME");
 						double laenge = Double.parseDouble(csvRecord.get("Laenge").replace(",", "."));
 						double breite = Double.parseDouble(csvRecord.get("Breite").replace(",", "."));
 						System.out.println(name+", "+ laenge +", "+ breite);
 						return new BahnhofInfo(name, laenge, breite);
+					}else{
+						throw new Exception("DS100 "+ ds100 + " is not part of Fernverkehr");
 					}
 				}
 			}
-
+			System.out.println("DS100 "+ dscode + " not in database");
+			throw new Exception("DS100 "+ dscode + " not in database");
 		} catch (IOException e) {
-            e.printStackTrace();
-        }
+			e.printStackTrace();
+		}
 		return null;
     }
 
+	/**
+	Function computeDistance
+	Input: 2 BahnhofInfo record (e.g. BahnhofInfo[name=Frankfurt(Main)Hbf, laenge=8.663789, breite=50.107145])
+	Output: long roundedDistance = computed 
+	*/
 	private long computeDistance(BahnhofInfo fromBhf, BahnhofInfo toBhf) {
+		/*
 		System.out.println("computeDistance(");
 		System.out.println(fromBhf.toString());
 		System.out.println(toBhf.toString());
 		System.out.println(")");
+		*/
 
 		// Convert latitude and longitude from degrees to radians
 		double latRad1 = Math.toRadians(fromBhf.breite());
@@ -72,15 +91,29 @@ public class DistanceController {
 
 		// Haversine formula with Mean Earth Radius
 		double meanEarthRadius = 6371.0088; // in kilometers
-        double dlat = latRad2 - latRad1;
-        double dlon = lonRad2 - lonRad1;
-        double a = Math.sin(dlat / 2) * Math.sin(dlat / 2)
-                + Math.cos(latRad1) * Math.cos(latRad2) * Math.sin(dlon / 2) * Math.sin(dlon / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = meanEarthRadius * c;
+		double dlat = latRad2 - latRad1;
+		double dlon = lonRad2 - lonRad1;
+		double a = Math.sin(dlat / 2) * Math.sin(dlat / 2)
+				+ Math.cos(latRad1) * Math.cos(latRad2) * Math.sin(dlon / 2) * Math.sin(dlon / 2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		double distance = meanEarthRadius * c;
 
-		long roundedDist=Math.round(distance);
-		System.out.println("roundedDist= "+ roundedDist);
-		return roundedDist;
+		// Round the computed distance (to whole km)
+		long roundedDistance=Math.round(distance);
+		System.out.println("roundedDistance= "+ roundedDistance+
+			"\n_____________________________________");
+
+		return roundedDistance;
+    }
+
+	/**
+	Function handleException
+	Input: caught Exception
+	Output: Error message to be read on the web page
+	*/
+	@ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        String errorMessage = "An error occurred: " + e.getMessage();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
     }
 }
