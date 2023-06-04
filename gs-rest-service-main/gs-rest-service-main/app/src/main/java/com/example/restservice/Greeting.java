863e4b7e285339db0c0a83255968da0d4b8a3726
Randria, Joshua - (507927) <joshua.randria@alstomgroup.com>
2023-06-04 14:52:46 +0200

    gs

---
+++ gs-rest-service-main/gs-rest-service-main/app/src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,8 @@
+package com.example.restservice;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Greeting(long id, String content) {
+
+ }
